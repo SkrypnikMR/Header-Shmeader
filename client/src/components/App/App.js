9769b9872312eebf333efde2d2f949d3b4c46d1b
SkripnikProgram <skripnikmrw@gmail.com>
2021-06-04 13:34:53 +0300

    create linter config

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react';
-import Header from '../Header';
-import ToDo from '../ToDo';
 
 
 class App extends Component {
-    render() {
-        return (
-            <>
-            </>
-        )
-    }
+  
 }
 
 export default App;
