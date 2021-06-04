86aea0339c9ee0264bdc2ec5c3d519984a9b8d15
SkripnikProgram <skripnikmrw@gmail.com>
2021-06-04 10:42:09 +0300

    base arch of our project

---
+++ server/app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const PORT = process.env.PORT || 1000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`app start on port:${PORT}`));
