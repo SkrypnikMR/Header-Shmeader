f8856d0078de5154061c5e00160a1675019a77f9
Mozzi22 <nn19092001@gmail.com>
2021-06-09 16:36:17 +0300

    deleted comments

--- client/src/components/App/UI/Input/tests/Input.test.js
+++ client/src/components/App/UI/Input/tests/Input.test.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import Input from '../Input'
 
-// jest.mock('../../ToDo', ()=>()=><div id="ToDo"></div>)
 describe('Input', () => {
   it('Should match snapshot', () => {
       const component = shallow(<Input />)
       expect(component.html()).toMatchSnapshot()
   })
-  // it('Should have Button ', () => {
-  //     const component = mount(<Button />)
-  //     expect(component.find('button')).toHaveLength(1)
-  // })
-  // it('Should call function from props on click on the button', () => {
-  //     const btnOnClick = jest.fn();
-  //     const title = 'testTitle';
-  //     const component = mount(<Button onButtonClick={btnOnClick} title={title} />)
-  //     component.simulate('click');
-  //     expect(btnOnClick).toHaveBeenCalled();
-  //     expect(component.html()).toBe(`<button>${title}</button>`);
-  // })
 })
