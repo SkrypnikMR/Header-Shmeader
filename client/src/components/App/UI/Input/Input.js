da05d1f9966d6ff2c344077c093ebb36807eddb1
Mozzi22 <nn19092001@gmail.com>
2021-06-09 16:21:25 +0300

    adding a folder for custom items.implementation of a custom input element. adding top styles for input.

---
+++ client/src/components/App/UI/Input/Input.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import classes from './Input.css'
+
+const Input = props => {
+  const cls = [classes.Input]
+
+  return(
+    <div className={cls.join(' ')}>
+      <label htmlFor={htmlFor}>{props.label}</label>
+      <input 
+        type="text"
+        value={props.value}
+        onChange={props.onChange}
+        placeholder={props.placeholder}
+      />
+      <span>{props.errorMessage}</span>
+    </div>
+  )
+}
+
+export default Input
