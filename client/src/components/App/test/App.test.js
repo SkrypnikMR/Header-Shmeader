0f95b8d6bfb414995c80360a7db308bbf0b08281
Anastasiia <khramovanastasiia@gmail.com>
2021-06-09 22:27:07 +0300

    example tests

---
+++ client/src/components/App/test/App.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import App from '../App'
+
+describe('App', () => {
+    it('Should match snapshot', () => {
+        const component = shallow(<App/>)
+        expect(component.html()).toMatchSnapshot()
+    })
+    it('Should have App ', () => {
+        const component = mount(<App/>)
+        expect(component.find('App')).toHaveLength(1)
+    })
+})
