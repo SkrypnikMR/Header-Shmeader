03fbcbc8ed9673d80724abced51ac673f5669fba
Anastasiia <khramovanastasiia@gmail.com>
2021-06-10 23:43:25 +0300

    documentation and some files

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,22 @@
-import '../public/styles/styles.scss';
+// import '../public/styles/styles.scss';
+// import React from 'react';
+// import { render } from 'react-dom';
+// import App from './components/App';
+
+// render(
+//     <App />,
+//     document.getElementById('root'),
+// );
+
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import App from './components/App';
+import reducers from './redux/rootReducer';
 
-render(
-    <App />,
-    document.getElementById('root'),
-);
+const store = createStore(reducers, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__()));
+
+render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
