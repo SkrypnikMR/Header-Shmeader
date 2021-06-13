a6f1b0f7345ea560fbe81cb0cbb49bf49bc9ccf1
SkrypnikMR <Skripnikmrw@gmail.com>
2021-06-13 17:29:39 +0300

    fix regInputs maping. now without return

--- client/src/components/Registration/Registration.jsx
+++ client/src/components/Registration/Registration.jsx
@@ -10,22 +10,20 @@ const Registration = (props) => {
     return (
         <Form>
             <p>Sign Up</p>
-            {regInputs.map((input) => {
-                return (
-                    <Input
-                        width="80%"
-                        id={input.id}
-                        key={input.id}
-                        height="100px"
-                        inputHeight="50px"
-                        borderRadius="5px"
-                        label={input.label}
-                        value={props[input.id]}
-                        onChange={props[`${input.id}Func`]}
-                        placeholder={input.placeholder}
-                    />
-                );
-            })}
+            {regInputs.map(input => (
+                <Input
+                    width="80%"
+                    id={input.id}
+                    key={input.id}
+                    height="100px"
+                    inputHeight="50px"
+                    borderRadius="5px"
+                    label={input.label}
+                    value={props[input.id]}
+                    onChange={props[`${input.id}Func`]}
+                    placeholder={input.placeholder}
+                />
+            ))}
             <Button id="registration" onClick={sendRegistrationRequest} content="Submit" height="50px" />
         </Form>
     );
