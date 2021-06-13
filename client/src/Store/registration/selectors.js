adcff4687293fde03a936608ad736a3a3f384ec6
SkrypnikMR <Skripnikmrw@gmail.com>
2021-06-14 01:07:37 +0300

    added destructurisation in registration selector regValues

--- client/src/Store/registration/selectors.js
+++ client/src/Store/registration/selectors.js
@@ -3,10 +3,5 @@ import { createSelector } from 'reselect';
 export const registrationStore = state => state.registration;
 export const regValues = createSelector(
     registrationStore,
-    registration => ({
-        email: registration.email,
-        password: registration.password,
-        firstName: registration.firstName,
-        lastName: registration.lastName,
-    }),
+    ({ email, password, lastName, firstName }) => ({ email, password, lastName, firstName }),
 );
