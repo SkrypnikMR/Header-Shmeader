b48cdc679f8595c4a0134d38a0ea4c1d364393a5
SkrypnikMR <Skripnikmrw@gmail.com>
2021-06-14 01:12:12 +0300

    fix name in selectors describe and add beforeEach block with initialState

--- client/src/Store/registration/tests/selectors.test.js
+++ client/src/Store/registration/tests/selectors.test.js
@@ -1,6 +1,17 @@
 import * as selectors from '../selectors';
 
-describe('selectors', () => {
+describe('registration selectors', () => {
+    let state;
+    beforeEach(() => {
+        state = {
+            registration: {
+                email: '',
+                password: '',
+                firstName: '',
+                lastName: '',
+            },
+        };
+    });
     describe('selectors.registrationStore', () => {
         it('toBe defined', () => {
             expect(selectors.registrationStore).toBeDefined();
@@ -9,14 +20,6 @@ describe('selectors', () => {
             expect(typeof selectors.registrationStore).toBe('function');
         });
         it('should return value', () => {
-            const state = {
-                registration: {
-                    email: '',
-                    password: '',
-                    firstName: '',
-                    lastName: '',
-                },
-            };
             expect(selectors.registrationStore(state)).toEqual(state.registration);
         });
     });
