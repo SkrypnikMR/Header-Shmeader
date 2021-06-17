8d7a6357f0c5e90554de5656a5a07a656743eb1a
SkrypnikMR <Skripnikmrw@gmail.com>
2021-06-17 20:24:40 +0300

    repair validation

--- server/helpers/validation.js
+++ server/helpers/validation.js
@@ -1,36 +1,16 @@
 const regExp = require('../Const/regExp')
 
 module.exports = {
-    registrationValidation: (req, res, next) => {
-        const { email, password, firstName, lastName } = req.body;
-        let answer = { message: '', isValid: true };
-
-        if (!regExp.emailRegExp.test(email) || !email) {
-            answer = { message: 'Invalid email', isValid: false };
-        }
-        if (!regExp.passwordRegExp.test(password) || !password) {
-            answer = { message: 'Invalid password', isValid: false };
-        }
-        if (!regExp.firstNameRegExp.test(firstName) || !firstName) {
-            answer = { message: 'Invalid firstName', isValid: false };
-        }
-        if (!regExp.lastNameRegExp.test(lastName) || !lastName) {
-            answer = { message: 'Invalid lastName', isValid: false };
-        }
-        req.validation = {message: answer.message, isValid: answer.isValid}
-        next();
+    registrationValidation: ({ email, password, firstName, lastName }) => {
+        if (!regExp.emailRegExp.test(email)) return { message: 'Invalid email', isValid: false };
+        if (!regExp.passwordRegExp.test(password)) return { message: 'Invalid password', isValid: false };
+        if (!regExp.firstNameRegExp.test(firstName)) return { message: 'Invalid firstName', isValid: false };
+        if (!regExp.lastNameRegExp.test(lastName)) return { message: 'Invalid lastName', isValid: false };
+        return { message: '', isValid: true };
     },
-    loginValidation: (req, res, next) => {
-        const { email, password } = req.body;
-        let answer = { message: '', isValid: true };
-
-        if (!regExp.emailRegExp.test(email) || !email) {
-            answer = { message: 'Invalid email', isValid: false };
-        }
-        if (!regExp.passwordRegExp.test(password) || !password) {
-            answer = { message: 'Invalid password', isValid: false };
-        }
-        req.validation = {message: answer.message, isValid: answer.isValid}
-        next();
+    loginValidation: ({ email, password }) => {
+        if (!regExp.emailRegExp.test(email)) return { message: 'Invalid email', isValid: false };
+        if (!regExp.passwordRegExp.test(password)) return { message: 'Invalid password', isValid: false };
+        return { message: '', isValid: true };
     },
 };
--- server/routes/account.js
+++ server/routes/account.js
@@ -4,11 +4,9 @@ const connection = require('../Controllers/Connector');
 const AccountManager = require('../Classes/AccountManager');
 const accountManager = new AccountManager(connection);
 const { registration, login } = accountManager;
-const validation = require('../helpers/validation');
-const { registrationValidation, loginValidation } = validation
 
-router.post('/registration', registrationValidation, registration);
-router.post('/login', loginValidation, login);
+router.post('/registration', registration);
+router.post('/login', login);
 
 
 module.exports = router;
