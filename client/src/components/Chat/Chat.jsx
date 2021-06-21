9a78cf45e4375e378ca7db594dee79a56050d485
SkrypnikMR <Skripnikmrw@gmail.com>
2021-06-22 01:05:49 +0300

    remove test socket io connect from Chat component. Was added only for test, in future i think we did it somewhre in redux

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { io } from 'socket.io-client';
 import ChatList from './ChatList';
 import ChatContent from './ChatContent';
 import { StChat } from './styled';
 
-io('http://localhost:2282', { path: '/socket' });
 const Chat = () => {
   return (
     <StChat>
