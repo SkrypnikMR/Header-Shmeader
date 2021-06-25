31f4bf9eb0f58d76e11a6fee77def14d0d8669f1
Rashevskyyy <keepcalm312000@gmail.com>
2021-06-25 13:19:45 +0300

    add test in headerControlPanel

--- client/src/components/Header/HeaderControlPanel/tests/HeaderControlPanel.test.js
+++ client/src/components/Header/HeaderControlPanel/tests/HeaderControlPanel.test.js
@@ -10,17 +10,46 @@ jest.mock('react-i18next', () => ({
 
 describe('HeaderControlPanel', () => {
     const { i18n } = useTranslation();
+    const props = {
+        history: [],
+        location: { pathname: '/' },
+    };
     it('Should match snapshot', () => {
-        const component = shallowSmart(<HeaderControlPanel />);
+        const component = shallowSmart(<HeaderControlPanel {...props} />);
         expect(component.html()).toMatchSnapshot();
     });
-    it('should render inputs', () => {
-        const component = mountSmart(<HeaderControlPanel />);
+    it('should render buttons', () => {
+        const component = mountSmart(<HeaderControlPanel {...props} />);
         expect(component.find('button')).toHaveLength(3);
     });
     it('should call i18n.changeLanguage ', () => {
-        const component = mountSmart(<HeaderControlPanel />);
+        const component = mountSmart(<HeaderControlPanel {...props} />);
         component.find('Button').first().simulate('click');
         expect(i18n.changeLanguage).toHaveBeenCalled();
+    });
+    it('should render all buttons in /chat', () => {
+        const props = {
+            history: [],
+            location: { pathname: '/chat' },
+        };
+        const component = mountSmart(<HeaderControlPanel {...props} />);
+        expect(component.find('button')).toHaveLength(4);
+    });
+    it('should render all buttons in /account', () => {
+        const props = {
+            history: [],
+            location: { pathname: '/account' },
+        };
+        const component = mountSmart(<HeaderControlPanel {...props} />);
+        expect(component.find('button')).toHaveLength(4);
+    });
+    it('should call history push', () => {
+        const props = {
+            history: { push: jest.fn() },
+            location: { pathname: '/chat' },
+        };
+        const component = mountSmart(<HeaderControlPanel {...props} />);
+        component.find('Button').at(3).getElement().props.onClick();
+        expect(props.history.push).toHaveBeenCalledWith('/account');
     });
 });
